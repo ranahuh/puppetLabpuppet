bc4446514a65d79b664053787cfbd6e1cd7e0974
ranahuh <reachganeshrana@hotmail.com>
2017-02-15 01:02:26 +0530

    This is the first module file

---
+++ module.pp
@@ -0,0 +1,8 @@
+###################################################################
+# This puppet file will create the users in linux machine 
+#
+# Puppet File: useradd.pp
+# Purpose: This puppet file will create the users in linux machine
+# Author: Gary Wilson
+#
+###################################################################
